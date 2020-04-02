8d243857f5bfba0662a140f5401b6ec67e24db53
Natalie Kass-Kaufman <kasskauf@adobe.com>
2020-04-01 20:28:43 -0400

    updated code

--- js/app.js
+++ js/app.js
@@ -33,21 +33,38 @@ $(function () {
 
   function search (searchTerm) {
     const url = 'https://api.giphy.com/v1/gifs/search'
-    const apiKey = '[ADD YOUR GIPHY API KEY HERE]'
+    const apiKey = 'n6ZEyjWqWxyrYK7pf3FoV9u8hdGD8EH9'
 
-    $.ajax({
-      url: url,
-      type: 'GET',
-      data: { q: searchTerm, limit: 50, api_key: apiKey }
+    axios.get(url, {
+      params: {
+        api_key: apiKey,
+        q: searchTerm,
+        limit: 50
+      }
     })
-    .done((response) => {
-      // execute this function if request is successful
-      console.log(response)
-      displayResults(response.data)
+    .then((response) => {
+      console.log(response.data.data)
+      displayResults(response.data.data)
     })
-    .fail(() => {
-      // execute this function if request fails
-      alert('error occurred')
+    .catch((error) => {
+      console.log(error)
+      console.log('something has gone wrong')
     })
+
+    //
+    // $.ajax({
+    //   url: url,
+    //   type: 'GET',
+    //   data: { q: searchTerm, limit: 50, api_key: apiKey }
+    // })
+    // .done((response) => {
+    //   // execute this function if request is successful
+    //   console.log(response)
+    //   displayResults(response.data)
+    // })
+    // .fail(() => {
+    //   // execute this function if request fails
+    //   alert('error occurred')
+    // })
   }
 })
